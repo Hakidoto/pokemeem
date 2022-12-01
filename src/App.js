29dcac9435f9425d562f42d8a2cefcb8738e989a
Hadid Garcia <hadid.garcia1@gmail.com>
2022-12-01 00:43:36 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-3 left">
+          <div className="core text-center mx-auto">
+            <p>Main screen</p>
+          </div>
+          <img
+            className="pkmnEnemy"
+            src="https://play.pokemonshowdown.com/sprites/ani-shiny/alakazam-mega.gif"
+          />
+          <img
+            className="pkmnAlly"
+            src="https://play.pokemonshowdown.com/sprites/ani-back-shiny/kecleon.gif"
+          ></img>
+          <div className="playerBox text-start mx-auto">
+            <p>Que deberia hacer?</p>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="Boton designado para elegir un movimiento"
+              >
+                Fight
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="Boton designado para elegir un item"
+              >
+                Bag
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-6 middle">
+          <div className="core text-center mx-auto">
+            <p>Battle log</p>
+          </div>
+          <div className="battleLog text-center mx-auto">
+            <p>Despliega el historial de acciones realizadas.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
