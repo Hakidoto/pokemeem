f5942807c4d097a593e1ce67943f297cb6b217d1
Hadid Garcia <hadid.garcia1@gmail.com>
2022-12-05 22:44:38 -0600

    getPokemon: retornar mas datos
    Signed-off-by: Hadid Garcia <hadid.garcia1@gmail.com>

--- src/Helpers/getPokemonApi.js
+++ src/Helpers/getPokemonApi.js
@@ -5,6 +5,10 @@ export const getPokemonApi = async (numPokemon) => {
   const results = await respuesta.json();
   const name = await results.name;
   const img = await results.sprites.other.dream_world.front_default;
+  const moves = await results.moves.map(move => move.move.name).slice(0, 10).join(', ');
+  const hp = await  results.stats[0].base_stat;
+  const id = results.id;
+  console.log(moves)
   console.log(img);
-  return { name, img };
+  return { name, img, moves, hp, id };
 };
