a01f2dd9a5d14b3104e11123b2116c7e4d106162
TaniaNaalB <97367204+TaniaNaalB@users.noreply.github.com>
2022-12-05 22:46:24 -0600

    Diseño v4

--- src/App.js
+++ src/App.js
@@ -37,99 +37,130 @@ useEffect(() => {
 
 
 return (
-      <div className="bg-dark text-light main">
-        <div className="playground">
-          <div className="titilePlayground">
-            <p className="">Patio de juegos</p>
-          </div>
-          <div className="charas">
-            <div className="enemyContainer">
-            {pokemonEnemy.map(({ name, id }) => (
-              <img
-                className="enemy"
-                src={`https://play.pokemonshowdown.com/sprites/ani-shiny/${name}.gif`} /* Se puede utilizar el nombre extraido de pokeapi para cambiar la imagen del pokemon */
-                alt="pkmnEnemy"
-              />
-            ))}
-            </div>
-            <div className="allyContainer">
-            {pokemon.map(({ name }) => (
-            <img
-              className="ally"
-              src={`https://play.pokemonshowdown.com/sprites/ani-back-shiny/${name}.gif`}
-              alt="pkmAlly"
-            ></img>
-            ))}
-            </div>
-          </div>
-          <div className="playgroundFooter">
-            <p className="footerP">Que deberia hacer?</p>
-            <div className="botones">
-              <div>
-                <button
-                  type="button"
-                  className="btn btn-primary m-1 boton"
-                  /*data-toggle="tooltip"
-                  data-placement="top"*/
-                  title="Boton designado para elegir un movimiento"
-                >
-                  Fight
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-primary m-1 boton"
-                  /*data-toggle="tooltip"
-                  data-placement="top"*/
-                  title="Boton designado para elegir un item"
-                >
-                  Bag
-                </button>
+      <div>
+        <div>
+          <div className="bg-dark text-light main">
+            <div className="playground">
+              <div className="titilePlayground">
+                <p className="">Patio de juegos</p>
               </div>
-              <div>
-                <button
-                  type="button"
-                  className="btn btn-primary m-1 boton"
-                  /*data-toggle="tooltip"
-                  data-placement="top"*/
-                  title="Boton designado para elegir un movimiento"
-                >
-                  Fight
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-primary m-1 boton"
-                  /*data-toggle="tooltip"
-                  data-placement="top"*/
-                  title="Boton designado para elegir un item"
-                >
-                  Bag
-                </button>
-              </div>
-              
-            </div>
-          </div>
-        </div>
-
-        <div className="logg">
-          <div>
-            <div className="titleLog">
-              <p className="">Battle log</p>
-            </div>
-            <div className="logContent">
-              <div>
-                <div className="logContentTitle">
-                  <p>Despliega el historial de acciones realizadas.</p>
+              <div className="charas">
+                <div className="enemyContainer">
+                {  
+                  pokemonEnemy.map(({ name, id }) => (
+                    <>
+                    <div className="progress lifeBar">
+                      <div className="progress-bar bg-success" role="progressbar" aria-label="Basic example" style={{width: "25%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                      <img
+                      className="enemy"
+                      src={`https://play.pokemonshowdown.com/sprites/ani-shiny/${name}.gif`} /* Se puede utilizar el nombre extraido de pokeapi para cambiar la imagen del pokemon */
+                      alt="pkmnEnemy"
+                  />
+                    </>
+                  
+                ))}
                 </div>
-                <div className="contentLogContent">
-                  <div className="logList">
-                    <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
-                    <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
-                    <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
-                    
+                <div className="allyContainer">
+                {pokemon.map(({ name }) => (
+                  <>
+                    <div className="progress lifeBar">
+                      <div className="progress-bar bg-success" role="progressbar" aria-label="Basic example" style={{width: "75%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <img
+                    className="ally"
+                    src={`https://play.pokemonshowdown.com/sprites/ani-back-shiny/${name}.gif`}
+                    alt="pkmAlly"
+                    />
+                  </>
+                
+                ))}
+                </div>
+              </div>
+              <div className="playgroundFooter">
+                <p className="footerP">Que deberia hacer?</p>
+                <div className="botones">
+                  <div>
+                    <button
+                      type="button"
+                      className="btn btn-primary m-1 boton"
+                      /*data-toggle="tooltip"
+                      data-placement="top"*/
+                      title="Boton designado para elegir un movimiento"
+                    >
+                      PokeHabilidad1
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-primary m-1 boton"
+                      /*data-toggle="tooltip"
+                      data-placement="top"*/
+                      title="Boton designado para elegir un item"
+                    >
+                      PokeHabilidad2
+                    </button>
+                  </div>
+                  <div>
+                    <button
+                      type="button"
+                      className="btn btn-primary m-1 boton"
+                      /*data-toggle="tooltip"
+                      data-placement="top"*/
+                      title="Boton designado para elegir un movimiento"
+                    >
+                      PokeHabilidad3
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-primary m-1 boton"
+                      /*data-toggle="tooltip"
+                      data-placement="top"*/
+                      title="Boton designado para elegir un item"
+                    >
+                      PokeHabilidad4
+                    </button>
                   </div>
                   
                 </div>
               </div>
+            </div>
+                
+            <div className="logg">
+              <div>
+                <div className="titleLog">
+                  <p className="">Battle log</p>
+                </div>
+                <div className="logContent">
+                  <div>
+                    <div className="logContentTitle">
+                      <p>Despliega el historial de acciones realizadas.</p>
+                    </div>
+                    <div className="contentLogContent">
+                      <div className="overflow-auto logList">
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                        <p>Pokependej01 ataco a pokependej02 con pokehabilidad </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="footer">
+            <div class="spinner-border" role="status">
+              <span class="visually-hidden">Loading...</span>
             </div>
           </div>
         </div>
