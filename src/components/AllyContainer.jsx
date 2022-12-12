dfcecfb516392b3598bd4ad21923d8cbdbd16f90
David Peralta <19470212@campeche.tecnm.mx>
2022-12-11 23:50:25 -0600

    Problemas de key solucionados, log al 80%

--- src/components/AllyContainer.jsx
+++ src/components/AllyContainer.jsx
@@ -4,9 +4,9 @@ const AllyContainer = ({pokemon}) => {
   return (
     <div className="allyContainer">
         {pokemon.map(({ name,hp,id }) => (
-          <>
-            <div className="progress lifeBar">
-                <div className="progress-bar bg-success" role="progressbar" aria-label="Basic example" style={{width: "100%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+          <div key={0}>
+            <div key={1} className="progress lifeBar">
+                <div key={2} className="progress-bar bg-success" role="progressbar" aria-label="Basic example" style={{width: "100%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
             <img
             key={id} 
@@ -14,8 +14,8 @@ const AllyContainer = ({pokemon}) => {
             src={`https://play.pokemonshowdown.com/sprites/ani-back-shiny/${name}.gif`}
             alt="pkmAlly"
             />
-            <p>{hp}</p>
-          </>
+            <p key={3}>{hp}</p>
+          </div>
         
         ))}
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ButtonSpace from './ButtonSpace'
 
-const Footer = ({pokemon, processHealt, }) => {
+const Footer = ({pokemon, processHealt, handleClickLog}) => {
   return (
     <div className="playgroundFooter">
         <p className="footerP">Que deberia hacer?</p>
 
         {/* Lo que se me ocurre es que se podrian meter los moves en otro estado en forma de arreglo y ya recorrer ese en vez 
         del pokemon*/}
-        <ButtonSpace pokemon={pokemon} processHealt={processHealt} />
+        <ButtonSpace handleClickLog={handleClickLog} pokemon={pokemon} processHealt={processHealt} />
       </div>
   )
 }
