e306e5cd34afbd88903a4c258bbfb1ec1d848390
David Peralta <19470212@campeche.tecnm.mx>
2022-12-26 15:58:10 -0600

    Diseño final
    Co-authored-by: DarianaCastillo <97011199+DarianaCastillo@users.noreply.github.com>
    Co-authored-by: JoseFFdamian <96941629+JoseFFdamian@users.noreply.github.com>

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -6,22 +6,18 @@ que muestra los datos a detalle del combate. */
 
 const Log = ({pokemon, log, enemyName, conVida}) => {
   return (
-    <div className="logg">
-      <div>
-        <div className="titleLog">
-          <p className="">Battle log</p>
-        </div>
-        <div className="logContent">
-          <div>
-            <div className="logContentTitle">
-              <p className='titlecontentLog'>Despliega el historial de acciones realizadas.</p>
-            </div>
-            <LogList conVida={conVida} enemyName={enemyName} pokemon={pokemon} log={log} />
-          </div>
+    <div className="logg border">
+      <div className="row g-0 text-center border titleContainer">
+        <div className="col-sm-6 col-md-7 title1">Battle Log</div>
+        <div className="col-6 col-md-5 border title2 ">
+          <p className='title22'>Historial de acciones realizadas</p>
         </div>
       </div>
+      <LogList conVida={conVida} enemyName={enemyName} pokemon={pokemon} log={log} />
     </div>
   )
 }
 
 export default Log
+
+//
