eb3985c1cdf5810cc352c32ff93cccebfb02ca2f
David Peralta <19470212@campeche.tecnm.mx>
2022-12-29 02:08:04 -0600

    Log al 100%

--- src/components/LogList.jsx
+++ src/components/LogList.jsx
@@ -4,10 +4,10 @@ const LogList = ({log, conVida, conVidaAlly}) => {
   let i = 0;
 
   // Crea una referencia al último elemento de la lista
-  const lastLogItemRef = React.useRef(null);
+  const lastLogItemRef = useRef(null);
 
   // Ejecuta una función cada vez que se agrega un nuevo elemento a la lista
-  React.useEffect(() => {
+  useEffect(() => {
     // Si existe la referencia al último elemento de la lista, hace scroll hasta él
     if (lastLogItemRef.current) {
       lastLogItemRef.current.scrollIntoView({ behavior: "smooth" });
@@ -21,7 +21,12 @@ const LogList = ({log, conVida, conVidaAlly}) => {
         {log.map((item, index) => (
         // Asigna la referencia al último elemento de la lista al elemento actual
         <li ref={lastLogItemRef} className={`list-group-item ${conVida && index === log.length - 1 ? 'bg-primary bg-transition' : index !== log.length - 1 ? 'bg-dark bg-transition' : 'bg-success bg-transition'} text-light m-1`} key={i++}>
-          {(conVida && conVidaAlly) || index !== log.length - 1 ? `${item.name.charAt(0).toUpperCase() + item.name.slice(1)} atacó a ${item.target.charAt(0).toUpperCase() + item.target.slice(1)} con ${item.moves} inflingiendo ${item.damage} de daño` : `${item.name} derroto a ${item.target}`}
+          {(conVida && conVidaAlly) || index !== log.length - 1 ? 
+          `${item.name.charAt(0).toUpperCase() + item.name.slice(1)} atacó a ${item.target.charAt(0).toUpperCase() + item.target.slice(1)} con ${item.moves} inflingiendo ${item.damage} de daño` 
+          : 
+          conVidaAlly ? `${item.target.charAt(0).toUpperCase() + item.target.slice(1)} derroto a ${item.name.charAt(0).toUpperCase() + item.name.slice(1)}` 
+          : 
+          `${item.name.charAt(0).toUpperCase() + item.name.slice(1)} derroto a ${item.target.charAt(0).toUpperCase() + item.target.slice(1)}`}
         </li>
       ))}
         </ul>
